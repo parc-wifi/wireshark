parc-wifi/wireshark

commit 537934f16f2bc1316cb89cb245e237a4ea2d4636

Add support for passing NTP times to proto_tree_add_item() by specifying an encoding of ENC_TIME_NTP.
This increases the number of decimal places shown for NTP times (from 6 to 9),
so round the value to the nearest microsecond.  (I can't tell if NTP times are
ever more precise than a microsecond--this rounding is mainly to be closer to
the old behavior.)

Use proto_tree_add_item() for some NTP times.


git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@35840 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-ntp.c
@@ -659,10 +659,6 @@ dissect_ntp_std(tvbuff_t *tvb, proto_tree *ntp_tree, guint8 flags)
 	double		rootdelay;
 	double		rootdispersion;
 	guint32		refid_addr;
-	const guint8	*reftime;
-	const guint8	*org;
-	const guint8	*rec;
-	const guint8	*xmt;
 	const gchar	*buffc;
 	gchar           *buff;
 	int		i;
@@ -776,38 +772,22 @@ dissect_ntp_std(tvbuff_t *tvb, proto_tree *ntp_tree, guint8 flags)
 	/* Reference Timestamp: This is the time at which the local clock was
 	 * last set or corrected.
 	 */
-	reftime = tvb_get_ptr(tvb, 16, 8);
-	proto_tree_add_bytes_format(ntp_tree, hf_ntp_reftime, tvb, 16, 8,
-				   reftime,
-			           "Reference Clock Update Time: %s",
-				   ntp_fmt_ts(reftime));
+	proto_tree_add_item(ntp_tree, hf_ntp_reftime, tvb, 16, 8, ENC_TIME_NTP);
 
 	/* Originate Timestamp: This is the time at which the request departed
 	 * the client for the server.
 	 */
-	org = tvb_get_ptr(tvb, 24, 8);
-	proto_tree_add_bytes_format(ntp_tree, hf_ntp_org, tvb, 24, 8,
-				   org,
-			           "Originate Time Stamp: %s",
-				   ntp_fmt_ts(org));
+	proto_tree_add_item(ntp_tree, hf_ntp_org, tvb, 24, 8, ENC_TIME_NTP);
 
 	/* Receive Timestamp: This is the time at which the request arrived at
 	 * the server.
 	 */
-	rec = tvb_get_ptr(tvb, 32, 8);
-	proto_tree_add_bytes_format(ntp_tree, hf_ntp_rec, tvb, 32, 8,
-				   rec,
-			           "Receive Time Stamp: %s",
-				   ntp_fmt_ts(rec));
+	proto_tree_add_item(ntp_tree, hf_ntp_rec, tvb, 32, 8, ENC_TIME_NTP);
 
 	/* Transmit Timestamp: This is the time at which the reply departed the
 	 * server for the client.
 	 */
-	xmt = tvb_get_ptr(tvb, 40, 8);
-	proto_tree_add_bytes_format(ntp_tree, hf_ntp_xmt, tvb, 40, 8,
-				   xmt,
-			           "Transmit Time Stamp: %s",
-				   ntp_fmt_ts(xmt));
+	proto_tree_add_item(ntp_tree, hf_ntp_xmt, tvb, 40, 8, ENC_TIME_NTP);
 
 	/* MAX_MAC_LEN is the largest message authentication code
 	 * (MAC) length.  If we have more data left in the packet
@@ -1248,16 +1228,16 @@ proto_register_ntp(void)
 			"Reference Clock ID", "ntp.refid", FT_BYTES, BASE_NONE,
 			NULL, 0, NULL, HFILL }},
 		{ &hf_ntp_reftime, {
-			"Reference Clock Update Time", "ntp.reftime", FT_BYTES, BASE_NONE,
+			"Reference Clock Update Time", "ntp.reftime", FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC,
 			NULL, 0, NULL, HFILL }},
 		{ &hf_ntp_org, {
-			"Originate Time Stamp", "ntp.org", FT_BYTES, BASE_NONE,
+			"Originate Time Stamp", "ntp.org", FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC,
 			NULL, 0, NULL, HFILL }},
 		{ &hf_ntp_rec, {
-			"Receive Time Stamp", "ntp.rec", FT_BYTES, BASE_NONE,
+			"Receive Time Stamp", "ntp.rec", FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC,
 			NULL, 0, NULL, HFILL }},
 		{ &hf_ntp_xmt, {
-			"Transmit Time Stamp", "ntp.xmt", FT_BYTES, BASE_NONE,
+			"Transmit Time Stamp", "ntp.xmt", FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC,
 			NULL, 0, NULL, HFILL }},
 		{ &hf_ntp_keyid, {
 			"Key ID", "ntp.keyid", FT_BYTES, BASE_NONE,

epan/proto.c
@@ -1434,9 +1434,7 @@ proto_tree_new_item(field_info *new_fi, proto_tree *tree,
 				 * we made string values counted
 				 * rather than null-terminated.)
 				 */
-				string = tvb_get_ephemeral_string(tvb,
-									  start,
-									  length);
+				string = tvb_get_ephemeral_string(tvb, start, length);
 			}
 			new_fi->length = length;
 			proto_tree_set_string(new_fi, string);
@@ -1464,33 +1462,42 @@ proto_tree_new_item(field_info *new_fi, proto_tree *tree,
 
 		case FT_ABSOLUTE_TIME:
 		case FT_RELATIVE_TIME:
-			DISSECTOR_ASSERT(length == 8 || length == 4);
-			/*
-			 * NOTE: to support code written when
-			 * proto_tree_add_item() took a gboolean as its
-			 * last argument, with FALSE meaning "big-endian"
-			 * and TRUE meaning "little-endian", we treat any
-			 * non-zero value of "encoding" as meaning
-			 * "little-endian".
-			 *
-			 * At some point in the future, we might
-			 * support non-struct timespec formats in
-			 * the encoding as well (struct timeval,
-			 * NTP time, Windows NT FILETIME, time_t, etc.).
+			/* Historically, FT_TIMEs were only timespecs and 'encoding'
+			 * only specified if the timespec was stored in big- or
+			 * little-endian format.
 			 */
-			if (encoding) {
-				time_stamp.secs  = tvb_get_letohl(tvb, start);
-				if (length == 8)
-					time_stamp.nsecs = tvb_get_letohl(tvb, start+4);
-				else
-					time_stamp.nsecs = 0;
-			} else {
+			if (encoding == ENC_TIME_TIMESPEC_BE) { /* or FALSE/ENC_BIG_ENDIAN */
+				DISSECTOR_ASSERT(length == 8 || length == 4);
 				time_stamp.secs  = tvb_get_ntohl(tvb, start);
 				if (length == 8)
 					time_stamp.nsecs = tvb_get_ntohl(tvb, start+4);
 				else
 					time_stamp.nsecs = 0;
+			} else if (encoding == ENC_TIME_NTP) {
+				DISSECTOR_ASSERT(length == 8);
+				DISSECTOR_ASSERT(new_fi->hfinfo->display == ABSOLUTE_TIME_UTC);
+
+/* XXX - where should this go? */
+#define NTP_BASETIME 2208988800ul
+				time_stamp.secs  = tvb_get_ntohl(tvb, start);
+				if (time_stamp.secs)
+					time_stamp.secs -= NTP_BASETIME;
+
+				/* We're using nanoseconds here (and we will display nanoseconds),
+				 * but NTP's timestamps have a precision in microseconds or greater.
+				 * Round to 1 microsecond.
+				 */
+				time_stamp.nsecs = 1000000*(tvb_get_ntohl(tvb, start+4)/4294967296.0);
+				time_stamp.nsecs *= 1000;
+			} else { /* TRUE or ENC_LITTLE_ENDIAN/ENC_TIME_TIMESPEC_LE */
+				DISSECTOR_ASSERT(length == 8 || length == 4);
+				time_stamp.secs  = tvb_get_letohl(tvb, start);
+				if (length == 8)
+					time_stamp.nsecs = tvb_get_letohl(tvb, start+4);
+				else
+					time_stamp.nsecs = 0;
 			}
+
 			proto_tree_set_time(new_fi, &time_stamp);
 			break;
 

epan/dissectors/packet-3g-a11.c
@@ -46,8 +46,6 @@
 /* Include vendor id translation */
 #include <epan/sminmpec.h>
 
-#include "packet-ntp.h"
-
 /* Initialize the protocol and registered fields */
 static int proto_a11 = -1;
 static int hf_a11_type = -1;
@@ -1383,7 +1381,6 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     guint8        type;
     guint8        flags;
     guint         offset=0;
-    const guint8 *reftime;
 
     if (!tvb_bytes_exist(tvb, offset, 1))
         return 0;       /* not enough data to check message type */
@@ -1442,11 +1439,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8, reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1481,12 +1474,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
         } /* if tree */
 
@@ -1515,12 +1503,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1554,12 +1537,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1588,12 +1566,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1627,12 +1600,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1666,12 +1634,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1700,12 +1663,7 @@ dissect_a11( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             offset += 4;
 
             /* Identifier - assumed to be an NTP time here */
-            reftime = tvb_get_ptr(tvb, offset, 8);
-            proto_tree_add_bytes_format_value(a11_tree, hf_a11_ident, tvb,
-                                              offset, 8,
-                                              reftime,
-                                              "%s",
-                                              ntp_fmt_ts(reftime));
+            proto_tree_add_item(a11_tree, hf_a11_ident, tvb, offset, 8, ENC_TIME_NTP);
             offset += 8;
 
         } /* if tree */
@@ -1807,7 +1765,7 @@ proto_register_a11(void)
         },
         { &hf_a11_ident,
           { "Identification",           "a11.ident",
-            FT_BYTES, BASE_NONE, NULL, 0,
+            FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC, NULL, 0,
             "MN Identification.", HFILL }
         },
         { &hf_a11_ext_type,

epan/proto.h
@@ -205,6 +205,13 @@ typedef struct _protocol protocol_t;
  */
 #define ENC_BIG_ENDIAN		0x00000000
 #define ENC_LITTLE_ENDIAN	0x80000000
+/* Historically FT_TIMEs were only timespecs, the only question was whether
+ * they were stored in big- or little-endian format.  These macros use the
+ * big/little-endian values for backwards compatibility.
+ */
+#define ENC_TIME_TIMESPEC_BE	ENC_BIG_ENDIAN
+#define ENC_TIME_TIMESPEC_LE	ENC_LITTLE_ENDIAN
+#define ENC_TIME_NTP		0x00000002
 
 #define ENC_NA			0x00000000
 

commit 59182f68dfec1721c7cecc20d20b1286895818f6

Fix problem where NTP times with the high-bit set to 0 (which RFC 2030 chapter 3 has redefined to mean years *after* 2036) were being represented as times prior to 1968.
This has been broken since r35840 (apparently not many people see NTP
timestamps beyond 2036 :-)): apparently I over-optimized packet-ntp's code
while copying it into proto.c: that temporary variable is necessary for the
unsigned math to happen correctly before assigning the result to the (signed)
time_t.

Leave a comment in the code indicating why the temporary variable is needed.
Copy that comment to packet-ntp.c.

Fix the same problem in ntp_to_nstime(): it also did not use the temporary variable.


git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@45790 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-ntp.c
@@ -644,6 +644,10 @@ tvb_ntp_fmt_ts(tvbuff_t *tvb, gint offset)
 		return "NULL";
 	}
 
+	/* We need a temporary variable here so the unsigned math
+	 * works correctly (for years > 2036 according to RFC 2030
+	 * chapter 3).
+	 */
 	temptime = tempstmp - (guint32) NTP_BASETIME;
 	bd = gmtime(&temptime);
 	if(!bd){
@@ -666,9 +670,18 @@ tvb_ntp_fmt_ts(tvbuff_t *tvb, gint offset)
 void
 ntp_to_nstime(tvbuff_t *tvb, gint offset, nstime_t *nstime)
 {
-	nstime->secs  = tvb_get_ntohl(tvb, offset);
-	if (nstime->secs)
-		nstime->secs -= NTP_BASETIME;
+	guint32		 tempstmp;
+
+	/* We need a temporary variable here so the unsigned math
+	 * works correctly (for years > 2036 according to RFC 2030
+	 * chapter 3).
+	 */
+	tempstmp  = tvb_get_ntohl(tvb, offset);
+	if (tempstmp)
+		nstime->secs = tempstmp - (guint32)NTP_BASETIME;
+	else
+		nstime->secs = tempstmp; /* 0 */
+
 	nstime->nsecs = (int)(tvb_get_ntohl(tvb, offset+4)/(NTP_FLOAT_DENOM/1000000000.0));
 }
 

epan/proto.c
@@ -618,7 +618,7 @@ void
 proto_tree_reset(proto_tree *tree)
 {
 	tree_data_t *tree_data = PTREE_DATA(tree);
-  
+
 	proto_tree_children_foreach(tree, proto_tree_free_node, NULL);
 
 	/* reset tree */
@@ -1230,6 +1230,7 @@ proto_tree_new_item(field_info *new_fi, proto_tree *tree,
 	double	    doubleval;
 	const char *string;
 	nstime_t    time_stamp;
+	guint32     tmpsecs;
 	GPtrArray  *ptrs;
 	gboolean    length_error;
 
@@ -1626,9 +1627,16 @@ proto_tree_new_item(field_info *new_fi, proto_tree *tree,
 
 /* XXX - where should this go? */
 #define NTP_BASETIME 2208988800ul
-				time_stamp.secs  = tvb_get_ntohl(tvb, start);
-				if (time_stamp.secs)
-					time_stamp.secs -= NTP_BASETIME;
+
+				/* We need a temporary variable here so the unsigned math
+				 * works correctly (for years > 2036 according to RFC 2030
+				 * chapter 3).
+				 */
+				tmpsecs  = tvb_get_ntohl(tvb, start);
+				if (tmpsecs)
+					time_stamp.secs = tmpsecs - (guint32)NTP_BASETIME;
+				else
+					time_stamp.secs = tmpsecs; /* 0 */
 
 				if (length == 8) {
 					/*
@@ -1648,9 +1656,11 @@ proto_tree_new_item(field_info *new_fi, proto_tree *tree,
 				/*
 				 * NTP time stamp, big-endian.
 				 */
-				time_stamp.secs  = tvb_get_letohl(tvb, start);
-				if (time_stamp.secs)
-					time_stamp.secs -= NTP_BASETIME;
+				tmpsecs  = tvb_get_letohl(tvb, start);
+				if (tmpsecs)
+					time_stamp.secs = tmpsecs - (guint32)NTP_BASETIME;
+				else
+					time_stamp.secs = tmpsecs; /* 0 */
 
 				if (length == 8) {
 					/*